4bcbe842c30bed5a3f373b3859de334fe993df9d
Srikanth Srinivas <srikanth.srinivas@mail.utoronto.ca>
2020-05-03 14:12:18 -0400

    Fixed script.sh small issues

--- script.sh
+++ script.sh
@@ -24,15 +24,15 @@ git clone https://github.com/srikanthsrnvs/astrum.git
 PASSWORD: c8b755060cdc9feed578c0ec0b06886f641f3aab
 
 sudo mkdir models
-cd virtualenvironment/astrum/bin
-source activate
-cd
 cd astrum/source
 pip3 install -r requirements.txt
+
+cd
 
 echo "deb [arch=amd64] http://storage.googleapis.com/tensorflow-serving-apt stable tensorflow-model-server tensorflow-model-server-universal" | sudo tee /etc/apt/sources.list.d/tensorflow-serving.list && \
 curl https://storage.googleapis.com/tensorflow-serving-apt/tensorflow-serving.release.pub.gpg | sudo apt-key add -
 
 sudo apt-get update && sudo apt-get install tensorflow-model-server
 
+cd astrum/source
 sudo /virtualenv/astrum/bin/python3 spin_predictor.py
